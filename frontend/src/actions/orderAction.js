c82f9dfb2e56604d5178bfa4962b0728ba4d619d
Dipanshu Rai <dipanshurai933@gmail.com>
2025-07-27 13:28:44 +0530

    Add axiosInstance for backend call

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axiosInstance from "../utils/axiosInstance";
 import { ALL_ORDERS_FAIL, ALL_ORDERS_REQUEST, ALL_ORDERS_SUCCESS, CLEAR_ERRORS, DELETE_ORDER_FAIL, DELETE_ORDER_REQUEST, DELETE_ORDER_SUCCESS, MY_ORDERS_FAIL, MY_ORDERS_REQUEST, MY_ORDERS_SUCCESS, NEW_ORDER_FAIL, NEW_ORDER_REQUEST, NEW_ORDER_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, PAYMENT_STATUS_FAIL, PAYMENT_STATUS_REQUEST, PAYMENT_STATUS_SUCCESS, UPDATE_ORDER_FAIL, UPDATE_ORDER_REQUEST, UPDATE_ORDER_SUCCESS } from "../constants/orderConstants";
 
 // New Order
@@ -12,7 +12,7 @@ export const newOrder = (order) => async (dispatch) => {
             },
         };
 
-        const { data } = await axios.post('/api/v1/order/new', order, config);
+        const { data } = await axiosInstance.post('/api/v1/order/new', order, config);
 
         dispatch({
             type: NEW_ORDER_SUCCESS,
@@ -32,7 +32,7 @@ export const myOrders = () => async (dispatch) => {
     try {
         dispatch({ type: MY_ORDERS_REQUEST });
 
-        const { data } = await axios.get('/api/v1/orders/me');
+        const { data } = await axiosInstance.get('/api/v1/orders/me');
 
         dispatch({
             type: MY_ORDERS_SUCCESS,
@@ -52,7 +52,7 @@ export const getOrderDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: ORDER_DETAILS_REQUEST });
 
-        const { data } = await axios.get(`/api/v1/order/${id}`);
+        const { data } = await axiosInstance.get(`/api/v1/order/${id}`);
 
         dispatch({
             type: ORDER_DETAILS_SUCCESS,
@@ -72,7 +72,7 @@ export const getPaymentStatus = (id) => async (dispatch) => {
     try {
         dispatch({ type: PAYMENT_STATUS_REQUEST });
 
-        const { data } = await axios.get(`/api/v1/payment/status/${id}`);
+        const { data } = await axiosInstance.get(`/api/v1/payment/status/${id}`);
 
         dispatch({
             type: PAYMENT_STATUS_SUCCESS,
@@ -92,7 +92,7 @@ export const getAllOrders = () => async (dispatch) => {
     try {
         dispatch({ type: ALL_ORDERS_REQUEST });
 
-        const { data } = await axios.get('/api/v1/admin/orders');
+        const { data } = await axiosInstance.get('/api/v1/admin/orders');
 
         dispatch({
             type: ALL_ORDERS_SUCCESS,
@@ -118,7 +118,7 @@ export const updateOrder = (id, order) => async (dispatch) => {
             },
         };
 
-        const { data } = await axios.put(`/api/v1/admin/order/${id}`, order, config);
+        const { data } = await axiosInstance.put(`/api/v1/admin/order/${id}`, order, config);
 
         dispatch({
             type: UPDATE_ORDER_SUCCESS,
@@ -138,7 +138,7 @@ export const deleteOrder = (id) => async (dispatch) => {
     try {
         dispatch({ type: DELETE_ORDER_REQUEST });
 
-        const { data } = await axios.delete(`/api/v1/admin/order/${id}`);
+        const { data } = await axiosInstance.delete(`/api/v1/admin/order/${id}`);
 
         dispatch({
             type: DELETE_ORDER_SUCCESS,
--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -36,7 +36,7 @@ import {
     ALL_USERS_SUCCESS,
     ALL_USERS_REQUEST,
 } from '../constants/userConstants';
-import axios from 'axios';
+import axiosInstance from "../utils/axiosInstance";
 
 // Login User
 export const loginUser = (email, password) => async (dispatch) => {
@@ -50,7 +50,7 @@ export const loginUser = (email, password) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.post(
+        const { data } = await axiosInstance.post(
             '/api/v1/login',
             { email, password },
             config
@@ -84,7 +84,7 @@ export const registerUser = (userData) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.post(
+        const { data } = await axiosInstance.post(
             '/api/v1/register',
             userData,
             config
@@ -109,7 +109,7 @@ export const loadUser = () => async (dispatch) => {
 
         dispatch({ type: LOAD_USER_REQUEST });
 
-        const { data } = await axios.get('/api/v1/me');
+        const { data } = await axiosInstance.get('/api/v1/me');
 
         dispatch({
             type: LOAD_USER_SUCCESS,
@@ -127,7 +127,7 @@ export const loadUser = () => async (dispatch) => {
 // Logout User
 export const logoutUser = () => async (dispatch) => {
     try {
-        await axios.get('/api/v1/logout');
+        await axiosInstance.get('/api/v1/logout');
         dispatch({ type: LOGOUT_USER_SUCCESS });
     } catch (error) {
         dispatch({
@@ -149,7 +149,7 @@ export const updateProfile = (userData) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.put(
+        const { data } = await axiosInstance.put(
             '/api/v1/me/update',
             userData,
             config
@@ -180,7 +180,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.put(
+        const { data } = await axiosInstance.put(
             '/api/v1/password/update',
             passwords,
             config
@@ -212,7 +212,7 @@ export const forgotPassword = (email) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.post(
+        const { data } = await axiosInstance.post(
             '/api/v1/password/forgot',
             email,
             config
@@ -243,7 +243,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.put(
+        const { data } = await axiosInstance.put(
             `/api/v1/password/reset/${token}`,
             passwords,
             config
@@ -267,7 +267,7 @@ export const getAllUsers = () => async (dispatch) => {
     try {
 
         dispatch({ type: ALL_USERS_REQUEST });
-        const { data } = await axios.get('/api/v1/admin/users');
+        const { data } = await axiosInstance.get('/api/v1/admin/users');
         dispatch({
             type: ALL_USERS_SUCCESS,
             payload: data.users,
@@ -286,7 +286,7 @@ export const getUserDetails = (id) => async (dispatch) => {
     try {
 
         dispatch({ type: USER_DETAILS_REQUEST });
-        const { data } = await axios.get(`/api/v1/admin/user/${id}`);
+        const { data } = await axiosInstance.get(`/api/v1/admin/user/${id}`);
 
         dispatch({
             type: USER_DETAILS_SUCCESS,
@@ -313,7 +313,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
             },
         }
 
-        const { data } = await axios.put(
+        const { data } = await axiosInstance.put(
             `/api/v1/admin/user/${id}`,
             userData,
             config
@@ -337,7 +337,7 @@ export const deleteUser = (id) => async (dispatch) => {
     try {
 
         dispatch({ type: DELETE_USER_REQUEST });
-        const { data } = await axios.delete(`/api/v1/admin/user/${id}`);
+        const { data } = await axiosInstance.delete(`/api/v1/admin/user/${id}`);
 
         dispatch({
             type: DELETE_USER_SUCCESS,
