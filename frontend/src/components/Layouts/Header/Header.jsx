0e17d4237414443490a73cb887490745c8b15cfe
Dipanshu Rai <dipanshurai933@gmail.com>
2025-07-27 13:35:06 +0530

    Bug fix

--- frontend/src/components/Layouts/Header/Header.jsx
+++ frontend/src/components/Layouts/Header/Header.jsx
@@ -11,7 +11,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-  const { isAuthenticated, user } = useSelector((state) => state.user);
+  const { isAuthenticated, user } = useSelector((state) => state.user);  
 
   const { cartItems } = useSelector(state => state.cart);
 
@@ -42,7 +42,7 @@ const Header = () => {
             <Link to="/login" className="px-3 sm:px-9 py-0.5 text-primary-blue bg-white border font-medium rounded-sm cursor-pointer">Login</Link>
             :
             (
-              <span className="userDropDown flex items-center text-white font-medium gap-1 cursor-pointer" onClick={() => setTogglePrimaryDropDown(!togglePrimaryDropDown)}>{user.name && user.name.split(" ", 1)}
+              <span className="userDropDown flex items-center text-white font-medium gap-1 cursor-pointer" onClick={() => setTogglePrimaryDropDown(!togglePrimaryDropDown)}>{user?.name && user?.name.split(" ", 1)}
                 <span>{togglePrimaryDropDown ? <ExpandLessIcon sx={{ fontSize: "16px" }} /> : <ExpandMoreIcon sx={{ fontSize: "16px" }} />}</span>
               </span>
             )
